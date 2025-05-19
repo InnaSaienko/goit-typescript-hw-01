d4ddea4edba6cebed2a6e334e14983fa47e1f16f
Inna Saienko <inna.saienko.nl@gmail.com>
2025-05-19 09:32:46 +0200

    basic task 3 (add literal type)

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -2,4 +2,11 @@ type Union = {
     a: string | number;
 }
 
-const unionType : Union = { a: 25};
+const unionType: Union = {a: 25};
+
+type buttonType = 'enable' | 'disable';
+
+let buttonStatus: buttonType;
+
+buttonStatus = 'enable';
+buttonStatus = 'disable';
